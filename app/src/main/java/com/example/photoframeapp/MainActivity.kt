8183a48a0ff8e4df99c3a47a4d310539c48a791e
Nawab Singh Koli <nawabsinghkoli@gmail.com>
2024-11-01 13:02:02 +0530

    Photo Frame App Kotlin

---
+++ app/src/main/java/com/example/photoframeapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.photoframeapp
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var currentImage = 0
+    lateinit var image : ImageView
+    var names = arrayOf("Ratan Tata", "Abdul Kalam", "Kalpana Chawla", "Mary Kom")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        supportActionBar?.hide()
+
+        val prev = findViewById<ImageView>(R.id.imgPrev)
+        val next = findViewById<ImageView>(R.id.imgNext)
+        val text = findViewById<TextView>(R.id.textView)
+
+        prev.setOnClickListener {
+            val idCurrentImageString = "pic$currentImage"
+            // convert string id into integer address associated with it
+            val idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString, "id", packageName)
+            image = findViewById(idCurrentImageInt)
+            image.alpha = 0f
+
+            currentImage = (4+ currentImage-1)%4
+            val idImageToShowString = "pic$currentImage"
+            // convert string id into integer address associated with it
+            val idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
+            image = findViewById(idImageToShowInt)
+            image.alpha= 1f
+            text.text = names[currentImage]
+        }
+
+        next.setOnClickListener {
+            val idCurrentImageString = "pic$currentImage"
+            // convert string id into integer address associated with it
+            val idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString, "id", packageName)
+            image = findViewById(idCurrentImageInt)
+            image.alpha= 0f
+
+            currentImage = (4+ currentImage+1)%4
+            val idImageToShowString = "pic$currentImage"
+            // convert string id into integer address associated with it
+            val idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
+            image = findViewById(idImageToShowInt)
+            image.alpha= 1f
+            text.text = names[currentImage]
+        }
+    }
+}
